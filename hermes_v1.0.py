e5ca22b384601d4886d1f42a6f0aa7af98375767
unknown <giovannidelvecchio4@gmail.com>
2021-10-18 22:04:37 +0200

    Aggiunto il codice che manda anche le foto

--- hermes_v1.0.py
+++ hermes_v1.0.py
@@ -3,31 +3,36 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import xlrd
 import os.path
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-"""
-def send_alternating(number, text):
-    driver.get("https://web.whatsapp.com/send?phone=" + number + "&text=" + text)
-    time.sleep(10)
-    driver.find_element_by_class_name(button_class_name).click()
-    time.sleep(2)"""
+wa_button_send_id = "_4sWnG"
 
 
-def send_to_list(list_of_numbers, text_list):
-    button_class_name = "_4sWnG"
+def initialize_web_driver():
     homedir = os.path.expanduser("~")
     op = Options()
-    op.add_argument("--user-data-dir="+homedir+"\\AppData\\Local\\Google\\Chrome\\User Data")
+    op.add_argument("--user-data-dir=" + homedir + "\\AppData\\Local\\Google\\Chrome\\User Data")
     op.add_experimental_option("excludeSwitches", ["enable-automation"])
     op.add_experimental_option("useAutomationExtension", False)
 
     driver = webdriver.Chrome("chromedriver_win32//chromedriver.exe",
                               options=op)
     driver.maximize_window()
+    return homedir, op, driver
+
+
+def wa_send(driver):
+    driver.find_element_by_xpath("//span[@data-icon='send']").click()
+    time.sleep(2)
+
+
+def send_to_list(list_of_numbers, text_list):
+    homedir, op, driver = initialize_web_driver()
     for i in range(0, len(list_of_numbers)):
         driver.get("https://web.whatsapp.com/send?phone=" + list_of_numbers[i] + "&text=" + text_list[i])
         time.sleep(8)
-        driver.find_element_by_class_name(button_class_name).click()
-        time.sleep(2)
+        wa_send(driver)
     driver.close()
 
 
@@ -42,7 +47,34 @@ def acquire_numbers_and_text_from_excel_file(file_name = "numeri.xls"):
     return numbers, texts
 
 
-l_of_numbers, text_list = acquire_numbers_and_text_from_excel_file()
-send_to_list(list_of_numbers=l_of_numbers, text_list=text_list)
+def send_img():
+    homedir, op, driver = initialize_web_driver()
+    filepath = "C:\\Users\\GiovanniPio\\Desktop\\programmi vari\\point clouds\\bidone\\bidone00001.jpg"
+    dodo = "+393466296727"
+    clipboard_button = "_2jitM"
+    send = "_165_h _2HL9j"
+    text = "smegmabottle"
+
+    driver.get("https://web.whatsapp.com/send?phone=" + dodo + "&text=" + text)
+    time.sleep(5)
+    driver.find_element_by_class_name(clipboard_button).click()
+    #time.sleep(1)
+    inv = driver.find_element_by_xpath("//input[@type='file']")
+    inv.send_keys(filepath)
+    time.sleep(3)
+    #driver.find_elements_by_class_name(send)[0].click()
+    wa_send(driver)
 
 
+
+
+send_img()
+
+
+"""
+some saved urls
+
+#driver.get("https://www.google.com/search?q=lala&oq=lala&aqs=chrome..69i57.423j0j9&sourceid=chrome&ie=UTF-8")
+#driver.get("https://web.whatsapp.com/")
+#driver.find_element_by_class_name(cname).send_keys(Keys.control() + "v")
+"""
