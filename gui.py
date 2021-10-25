fd244ba600ce9e2784771c6b2645579f033c63af
Domenico Dell'Olio <zeito99@gmail.com>
2021-10-25 22:40:07 +0200

    abozzato tasto caricamento

--- gui.py
+++ gui.py
@@ -2,10 +2,19 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+from tkinter import filedialog, Tk
+from Options import Options
 
+options = None
 
 class MainWindow(Screen):
-    pass
+    filename = ObjectProperty(None)
+    def choose_file(self):
+        Tk().withdraw()
+        filepath = filedialog.askopenfilename(title="Scegli un File Excel",
+                                              filetypes=[("Excel files", ".xlsx .xls")])
+
+        options.set_exc_path(filepath)
 
 class RecapWindow(Screen):
     pass
@@ -20,6 +29,7 @@ kv = Builder.load_file("starting_window.kv")
 
 class BaseApp(App):
     def build(self):
+        options = Options()
         return kv
 
 if __name__ == '__main__':
