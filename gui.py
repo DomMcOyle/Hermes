b91b56388af5ca87ccb1e9cbe0a55691f067020c
Domenico Dell'Olio <zeito99@gmail.com>
2021-10-28 00:28:59 +0200

    Terminato funzionamento tasto di caricamento e tasto di preview

--- gui.py
+++ gui.py
@@ -4,33 +4,52 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from tkinter import filedialog, Tk
 from Options import Options
+import os
 
-options = None
 
 class MainWindow(Screen):
-    filename = ObjectProperty(None)
     def choose_file(self):
         Tk().withdraw()
         filepath = filedialog.askopenfilename(title="Scegli un File Excel",
                                               filetypes=[("Excel files", ".xlsx .xls")])
+        # TODO eventualmente controllare liceità file excel
+        app = App.get_running_app()
+        app.options.set_exc_path(filepath)
+        fnlabel = self.ids.filename_label
+        fnlabel.text = os.path.basename(filepath)
+        if self.ids.visualize_button.disabled:
+            self.ids.visualize_button.disabled = False
 
-        options.set_exc_path(filepath)
+
+    def preview_excel(self):
+        app = App.get_running_app()
+        os.startfile(app.options.get_exc_path())
+
+
 
 class RecapWindow(Screen):
     pass
 
+
 class ProgressWindow(Screen):
     pass
+
 
 class WindowManager(ScreenManager):
     pass
 
-kv = Builder.load_file("starting_window.kv")
 
 class BaseApp(App):
     def build(self):
-        options = Options()
-        return kv
+        self.options = Options()
+        if not os.path.isfile(self.options.get_exc_path()):
+            # if the file does not exist anymore, the path is disabled.
+            self.options.set_exc_path("")
+        return Builder.load_file("starting_window.kv")
+
+    def stop(self, *args):
+        self.options.dump_options()
+
 
 if __name__ == '__main__':
     BaseApp().run()
