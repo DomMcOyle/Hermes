7c038f72bbf51fb3725b4668defe4ef02e21d1f8
Domenico Dell'Olio <zeito99@gmail.com>
2021-11-08 21:51:58 +0100

    aggiornato placehoder

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ wrong_number_exception_str = "wrong number exception"
 no_numbers_in_excel_exception_str = "no numbers in excel file"
 check_if_digit = "[0-9]"
 num_digits_in_number = 10
-place_holder_image = "place_holder_donore_come_manfredi.PNG"
+place_holder_image = "placeholder.PNG"
 
 except_message_timeout_reached = "timeout reached"
 except_message_qr = "qr phase not completed"
